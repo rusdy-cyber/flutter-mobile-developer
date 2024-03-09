11173a2bb8e48d3c0fce810f38319025de880e1e
rusdy-cyber <bengkalis.qouo@gmail.com>
2024-03-09 19:20:07 +0700

    dasar dart

---
+++ P01-pengenalan-dart/operator/run.dart
@@ -0,0 +1,24 @@
+// Operator Aritmatika
+void main() {
+  // penjumlahan
+  var penjumlahan = 1 + 2;
+  print(penjumlahan);
+  // pengurangan
+  var pengurangan = 3 - 2;
+  print(pengurangan);
+  // perkalian
+  var perkalian = 4 * 3;
+  print(perkalian);
+  // pembagian
+  var pembagian = 4 / 2;
+  print(pembagian);
+  // module
+  var module = 100 % 20;
+  print(module);
+  
+  // equality dan relational operator
+  int a = 239;
+  int b = 349;
+  print(a > b);
+  print(a < b);
+}
