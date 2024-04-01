18478208cefaccaa8fd0d21841e5edaa545348ee
rusdy-cyber <bengkalis.qouo@gmail.com>
2024-04-02 00:47:25 +0700

    update

--- absesnsi/lib/main.dart
+++ absesnsi/lib/main.dart
@@ -1,37 +1,54 @@
 import 'package:flutter/material.dart';
+import 'page_dua.dart'; // Import file halaman kedua
 
 void main() {
   runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+  const MyApp({Key? key});
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      debugShowCheckedModeBanner: false, //untuk menghilangkan tanda debug
+      debugShowCheckedModeBanner: false,
       home: Scaffold(
         body: SafeArea(
-          child: Center(
-          child: Container(
-            margin: EdgeInsets.only(left: 0.0, top: 0, right: 0, bottom: 0),
-            padding: EdgeInsets.only(left: 0, top: 270.0, right: 0, bottom: 0),
-        child: Column(
-          
-          children: <Widget>[
-            Image(
-              image: AssetImage('assets/images/logo.png'),
-              height: 100,
+          child: GestureDetector(
+            onVerticalDragEnd: (DragEndDetails details) {
+              if (details.primaryVelocity < 0) { // Geser ke atas
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(builder: (context) => PageDua()), // Navigasi ke halaman kedua
+                );
+              }
+            },
+            child: Center(
+              child: Container(
+                margin: EdgeInsets.all(0),
+                padding: EdgeInsets.only(top: 270.0),
+                child: Column(
+                  children: <Widget>[
+                    Image(
+                      image: AssetImage('assets/images/logo.png'),
+                      height: 100,
+                    ),
+                    SizedBox(height: 10),
+                    Text(
+                      "PresensiApp",
+                      style: TextStyle(
+                        fontSize: 23.0,
+                        fontWeight: FontWeight.bold,
+                        color: Colors.blue,
+                      ),
+                    ),
+                  ],
+                ),
               ),
-              SizedBox(height: 10),
-              Text("PresensiApp",
-                style: TextStyle(fontSize: 23.0,
-                  fontWeight: FontWeight.bold,
-                  color: Colors.blue))
-              ],
             ),
-          )))));
+          ),
+        ),
+      ),
+    );
   }
 }
-
--- absesnsi/lib/page_dua.dart
+++ absesnsi/lib/page_dua.dart
@@ -1,37 +1,36 @@
 import 'package:flutter/material.dart';
 
-void main() {
-  runApp(const MyApp());
-}
-
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+class PageDua extends StatelessWidget {
+  const PageDua({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false, //untuk menghilangkan tanda debug
-      home: Scaffold(
-        body: SafeArea(
-          child: Center(
+    return Scaffold(
+      body: SafeArea(
+        child: Center(
           child: Container(
             margin: EdgeInsets.only(left: 0.0, top: 20, right: 0, bottom: 0),
             padding: EdgeInsets.only(left: 0, top: 270.0, right: 0, bottom: 0),
-        child: Column(
-          
-          children: <Widget>[
-            Image(
-              image: AssetImage('assets/images/logo.png'),
-              height: 100,
-              ),
-              SizedBox(height: 10),
-              Text("PresensiApp",
-                style: TextStyle(fontSize: 23.0,
-                  fontWeight: FontWeight.bold,
-                  color: Colors.blue))
+            child: Column(
+              children: <Widget>[
+                Image(
+                  image: AssetImage('assets/images/logo.png'),
+                  height: 100,
+                ),
+                SizedBox(height: 10),
+                Text(
+                  "Page Dua",
+                  style: TextStyle(
+                    fontSize: 23.0,
+                    fontWeight: FontWeight.bold,
+                    color: Colors.blue,
+                  ),
+                )
               ],
             ),
-          )))));
+          ),
+        ),
+      ),
+    );
   }
 }
-
