373548d1d2b64ff03579e73ed142f599feca396c
rusdy-cyber <bengkalis.qouo@gmail.com>
2024-04-04 21:57:03 +0700

    update page3

--- absesnsi/lib/page_empat.dart
+++ absesnsi/lib/page_empat.dart
@@ -17,7 +17,8 @@ class PageEmpat extends StatelessWidget {
               children: [
                 CircleAvatar(
                   radius: 30,
-                  backgroundImage: AssetImage('assets/images/img_clip_path_group.png'),
+                  backgroundImage:
+                      AssetImage('assets/images/img_clip_path_group.png'),
                 ),
                 SizedBox(width: 10),
                 Column(
@@ -25,7 +26,8 @@ class PageEmpat extends StatelessWidget {
                   children: [
                     Text(
                       'Nama Pegawai',
-                      style: TextStyle(fontSize: 16, fontWeight: FontWeight.bold),
+                      style:
+                          TextStyle(fontSize: 16, fontWeight: FontWeight.bold),
                     ),
                     Text(
                       'Nama Departemen',
@@ -93,7 +95,8 @@ class PageEmpat extends StatelessWidget {
                           SizedBox(height: 4),
                           Text(
                             '07:00',
-                            style: TextStyle(fontSize: 16, fontWeight: FontWeight.bold),
+                            style: TextStyle(
+                                fontSize: 16, fontWeight: FontWeight.bold),
                           ),
                           SizedBox(height: 2),
                           Text(
@@ -141,7 +144,8 @@ class PageEmpat extends StatelessWidget {
                           SizedBox(height: 4),
                           Text(
                             '--:--',
-                            style: TextStyle(fontSize: 16, fontWeight: FontWeight.bold),
+                            style: TextStyle(
+                                fontSize: 16, fontWeight: FontWeight.bold),
                           ),
                           SizedBox(height: 2),
                           Text(
@@ -168,12 +172,155 @@ class PageEmpat extends StatelessWidget {
                 child: Text(
                   "Tekan untuk Persensi keluar",
                   style: TextStyle(
-                    fontSize: 12,
+                    fontSize: 15,
                     fontWeight: FontWeight.bold,
                     color: Color.fromARGB(255, 235, 235, 235),
                   ),
                 ),
               ),
+            ),
+            SizedBox(height: 15),
+            Row(
+              children: [
+                Expanded(
+                  child: Padding(
+                    padding: const EdgeInsets.symmetric(horizontal: 4.0),
+                    child: Container(
+                      height: 200,
+                      decoration: BoxDecoration(
+                        color: Color.fromARGB(255, 66, 67, 70),
+                        borderRadius: BorderRadius.circular(8),
+                        border: Border.all(
+                          color: Color.fromARGB(255, 134, 130, 130),
+                          width: 1,
+                        ),
+                      ),
+                      child: Column(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        crossAxisAlignment: CrossAxisAlignment.center,
+                        children: [
+                          Row(
+                            mainAxisAlignment: MainAxisAlignment.start,
+                            children: [
+                              Image.asset(
+                                'assets/images/icon.png',
+                                width: 30,
+                                height: 30,
+                              ),
+                              SizedBox(width: 4),
+                              Text(
+                                'Izin absen',
+                                style: TextStyle(
+                                    fontWeight: FontWeight.bold,
+                                    fontSize: 20,
+                                    color: Color.fromARGB(255, 255, 255, 255)),
+                              ),
+                            ],
+                          ),
+                          SizedBox(height: 4),
+                          Text(
+                            'isi form untuk ajuka absen',
+                            style: TextStyle(
+                                fontSize: 12,
+                                color: Color.fromARGB(255, 255, 255, 255)),
+                          ),
+                          SizedBox(height: 50),
+                          Align(
+                            alignment: Alignment.center,
+                            child: TextButton(
+                              onPressed: () {
+                                // Aksi saat tombol "absensi" ditekan
+                              },
+                              style: ElevatedButton.styleFrom(
+                                backgroundColor:
+                                    Color.fromARGB(255, 255, 255, 255),
+                              ),
+                              child: Text(
+                                "Tekan untuk Persensi keluar",
+                                style: TextStyle(
+                                  fontSize: 12,
+                                  fontWeight: FontWeight.bold,
+                                  color: Color.fromARGB(255, 7, 5, 5),
+                                ),
+                              ),
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                  ),
+                ),
+                SizedBox(width: 4),
+                Expanded(
+                  child: Padding(
+                    padding: const EdgeInsets.symmetric(horizontal: 4.0),
+                    child: Container(
+                      height: 200,
+                      decoration: BoxDecoration(
+                        color: Color.fromRGBO(121, 45, 156, 1),
+                        borderRadius: BorderRadius.circular(8),
+                        border: Border.all(
+                          color: Color.fromARGB(255, 134, 130, 130),
+                          width: 1,
+                        ),
+                      ),
+                      child: Column(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        crossAxisAlignment: CrossAxisAlignment.center,
+                        children: [
+                          Row(
+                            mainAxisAlignment: MainAxisAlignment.start,
+                            children: [
+                              Image.asset(
+                                'assets/images/icon.png',
+                                width: 30,
+                                height: 30,
+                              ),
+                              SizedBox(width: 4),
+                              Text(
+                                'Ajukan Cuti',
+                                style: TextStyle(
+                                  fontWeight: FontWeight.bold,
+                                  fontSize: 20,
+                                  color: Color.fromARGB(255, 255, 255, 255),
+                                ),
+                              ),
+                            ],
+                          ),
+                          SizedBox(height: 4),
+                          Text(
+                            'Isi  form untuk ajukan cuti',
+                            style: TextStyle(
+                                fontSize: 12,
+                                color: Color.fromARGB(255, 255, 255, 255)),
+                          ),
+                          SizedBox(height: 50),
+                          Align(
+                            alignment: Alignment.center,
+                            child: TextButton(
+                              onPressed: () {
+                                // Aksi saat tombol "absensi" ditekan
+                              },
+                              style: ElevatedButton.styleFrom(
+                                backgroundColor:
+                                    Color.fromARGB(255, 255, 255, 255),
+                              ),
+                              child: Text(
+                                "Tekan untuk Persensi keluar",
+                                style: TextStyle(
+                                  fontSize: 12,
+                                  fontWeight: FontWeight.bold,
+                                  color: Color.fromARGB(255, 7, 5, 5),
+                                ),
+                              ),
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                  ),
+                ),
+              ],
             ),
           ],
         ),
